6226fc843214bc804a5bc2245c1ece8b6deaafc8
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-04-25 12:42:53 +0200

    Remove temperature logging

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -25,18 +25,8 @@ void setup() {
     setupWebServer();
 }
 
-constexpr unsigned long TEMP_LOG_INTERVAL_MS = 3000;
-
 void loop() {
-    static unsigned long lastTempLogMs = 0;
-
     delay(16); // run at ~60Hz
 
     ledRuntimeTick();
-
-    const unsigned long now = millis();
-    if (now - lastTempLogMs >= TEMP_LOG_INTERVAL_MS) {
-        lastTempLogMs = now;
-        Serial.printf("CPU temp: %.2f C\n", temperatureRead());
-    }
 }
