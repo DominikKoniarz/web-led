6699b5c1d8947f030f3f1ab587f27db7a2f1628c
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-05-03 15:45:05 +0200

    Install tanstack query

--- web/src/layouts/root/root-layout.tsx
+++ web/src/layouts/root/root-layout.tsx
@@ -1,15 +1,16 @@
 import Header from "@/layouts/root/components/header";
 import NavigationTabs from "@/layouts/root/components/navgiation-tabs";
+import QueryClientProvider from "@/providers/query-client-provider";
 import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export function RootLayoutComponent() {
     return (
-        <>
+        <QueryClientProvider>
             <Header />
             <NavigationTabs />
             <Outlet />
             <TanStackRouterDevtools />
-        </>
+        </QueryClientProvider>
     );
 }
