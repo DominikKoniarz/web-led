52d9958629d42d2d4fe9c8dcee156059737562dd
Jakub Bizon <kubabizon97@gmail.com>
2026-05-10 17:52:54 +0200

    add wifi types

---
+++ web/src/pages/settings/types/wifi.ts
@@ -0,0 +1,37 @@
+type WifiScanStartedResponse = {
+    status: "started";
+};
+
+type WifiScanRunningResponse = {
+    status: "running";
+};
+
+type WifiScanCompleteResponse = {
+    status: "complete";
+    count: number;
+    networks: Network[];
+};
+
+export type WifiScanResponse =
+    | WifiScanStartedResponse
+    | WifiScanRunningResponse
+    | WifiScanCompleteResponse;
+
+type WifiAuth =
+    | "open"
+    | "wep"
+    | "wpa_psk"
+    | "wpa2_psk"
+    | "wpa_wpa2_psk"
+    | "wpa2_enterprise"
+    | "wpa3_psk"
+    | "wapi_psk"
+    | "auth_owe"
+    | "unknown";
+
+export type Network = {
+    ssid: string;
+    rssi: number;
+    channel: number;
+    auth: WifiAuth;
+};
