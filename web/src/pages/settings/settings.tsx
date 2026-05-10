3d54de1620ebb6b056231de1846bbceb86899de2
Jakub Bizon <kubabizon97@gmail.com>
2026-05-10 21:52:21 +0200

    add wifi status fetching function

--- web/src/pages/settings/settings.tsx
+++ web/src/pages/settings/settings.tsx
@@ -1,8 +1,32 @@
+import { env } from "@/env";
 import AvailableNetworks from "@/pages/settings/components/available-networks";
 import DeviceSettings from "@/pages/settings/components/device-settings";
 import SystemInfo from "@/pages/settings/components/system-info";
+import type { WifiStatus } from "@/pages/settings/types/wifi";
+import { useQuery } from "@tanstack/react-query";
 
 export function SettingsRouteComponent() {
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["wifi-status"],
+        queryFn: async () => {
+            const response = await fetch(
+                `${env.VITE_API_BASE_URL}/api/wifi/status`,
+                {
+                    headers: {
+                        Accept: "application/json",
+                    },
+                },
+            );
+            if (!response.ok) {
+                const message = await response.text();
+                throw new Error(
+                    `Request failed (${response.status}): ${message}`,
+                );
+            }
+            return (await response.json()) as WifiStatus;
+        },
+    });
+    console.log(data);
     return (
         <div className="container mx-auto space-y-6 px-4 py-6">
             <AvailableNetworks />
