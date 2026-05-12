29bf1167dc049fad3955bc3157cabc62cf9ec5a0
Jakub Bizon <kubabizon97@gmail.com>
2026-05-12 22:24:35 +0200

    fix skeleton

--- web/src/pages/settings/components/system-info-skeleton.tsx
+++ web/src/pages/settings/components/system-info-skeleton.tsx
@@ -12,15 +12,25 @@ export default function SystemInfoSkeleton() {
                 <div className="space-y-3 rounded-lg border p-4">
                     <div className="flex items-center justify-between">
                         <Skeleton className="h-4 w-28" />
-                        <Skeleton className="h-4 w-20" />
+                        <Skeleton className="h-4 w-16" />
                     </div>
                     <div className="flex items-center justify-between">
-                        <Skeleton className="h-4 w-16" />
-                        <Skeleton className="h-4 w-28" />
+                        <Skeleton className="h-4 w-10" />
+                        <Skeleton className="h-4 w-18" />
                     </div>
                     <div className="flex items-center justify-between">
                         <Skeleton className="h-4 w-24" />
                         <Skeleton className="h-4 w-16" />
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <Skeleton className="h-4 w-14" />
+                        <Skeleton className="h-4 w-22" />
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <Skeleton className="h-4 w-20" />
+                        <Skeleton className="h-4 w-14" />
                     </div>
                 </div>
 
